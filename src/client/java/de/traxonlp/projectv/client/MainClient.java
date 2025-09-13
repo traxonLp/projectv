cbd8ab73e05b7747b99a849d64fa413d6068d87d
traxonlp <traxonlp@github.com>
2025-09-13 23:45:05 +0200

    Initial Commit to GitHub Repo

---
+++ src/client/java/de/traxonlp/projectv/client/MainClient.java
@@ -0,0 +1,10 @@
+package de.traxonlp.projectv.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
